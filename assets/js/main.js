bedfc961ba59248e7deb1d825d9b052e5a80b41b
maeightyseven <consideritachallenge@gmail.com>
2024-04-17 23:51:29 +0200

    add fade center and pixel video

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,8 @@ var shapes = [];
 
 setInterval(function () {
     moving = 1;
-}, 1000)
+    video.play();  
+}, 50)
 
 function startGetLocation() {
     if (navigator.geolocation) {
@@ -203,8 +204,11 @@ function playCircle(x, y, r, a, n) {
          setTimeout(function () {
             distance = Number(measure(nowX, nowY, x, y));
             if (distance > r) {
-
-                  if ((!(a.paused)) && (a.classList[0] == 'playing')) {
+                if ($(a).parents(".circle").attr("fade") == "center") {
+                    a.volume = 0;
+                    a.classList.remove("playing");
+                }
+                else if ((!(a.paused)) && (a.classList[0] == 'playing')) {
                     a.classList.remove("playing");
                     playing.innerHTML = n + " pause audio " + distance + " from " + a.id;
                     decVolume(a);
@@ -346,4 +350,28 @@ function dowdots() {
     }, 500);
 }
 
+// Create the root video element
+var video = document.createElement('video');
+video.setAttribute('loop', '');
+// Add some styles if needed
+video.setAttribute('style', 'position: relative; width: 1px; height: 1px');
 
+// A helper to add sources to video
+function addSourceToVideo(element, type, dataURI) {
+    var source = document.createElement('source');
+    source.src = dataURI;
+    source.type = 'video/' + type;
+    element.appendChild(source);
+}
+
+// A helper to concat base64
+var base64 = function(mimeType, base64) {
+    return 'data:' + mimeType + ';base64,' + base64;
+};
+
+// Add Fake sourced
+addSourceToVideo(video,'webm', base64('video/webm', 'GkXfo0AgQoaBAUL3gQFC8oEEQvOBCEKCQAR3ZWJtQoeBAkKFgQIYU4BnQI0VSalmQCgq17FAAw9CQE2AQAZ3aGFtbXlXQUAGd2hhbW15RIlACECPQAAAAAAAFlSua0AxrkAu14EBY8WBAZyBACK1nEADdW5khkAFVl9WUDglhohAA1ZQOIOBAeBABrCBCLqBCB9DtnVAIueBAKNAHIEAAIAwAQCdASoIAAgAAUAmJaQAA3AA/vz0AAA='));
+addSourceToVideo(video, 'mp4', base64('video/mp4', 'AAAAHGZ0eXBpc29tAAACAGlzb21pc28ybXA0MQAAAAhmcmVlAAAAG21kYXQAAAGzABAHAAABthADAowdbb9/AAAC6W1vb3YAAABsbXZoZAAAAAB8JbCAfCWwgAAAA+gAAAAAAAEAAAEAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAIVdHJhawAAAFx0a2hkAAAAD3wlsIB8JbCAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAIAAAACAAAAAABsW1kaWEAAAAgbWRoZAAAAAB8JbCAfCWwgAAAA+gAAAAAVcQAAAAAAC1oZGxyAAAAAAAAAAB2aWRlAAAAAAAAAAAAAAAAVmlkZW9IYW5kbGVyAAAAAVxtaW5mAAAAFHZtaGQAAAABAAAAAAAAAAAAAAAkZGluZgAAABxkcmVmAAAAAAAAAAEAAAAMdXJsIAAAAAEAAAEcc3RibAAAALhzdHNkAAAAAAAAAAEAAACobXA0dgAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAIAAgASAAAAEgAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABj//wAAAFJlc2RzAAAAAANEAAEABDwgEQAAAAADDUAAAAAABS0AAAGwAQAAAbWJEwAAAQAAAAEgAMSNiB9FAEQBFGMAAAGyTGF2YzUyLjg3LjQGAQIAAAAYc3R0cwAAAAAAAAABAAAAAQAAAAAAAAAcc3RzYwAAAAAAAAABAAAAAQAAAAEAAAABAAAAFHN0c3oAAAAAAAAAEwAAAAEAAAAUc3RjbwAAAAAAAAABAAAALAAAAGB1ZHRhAAAAWG1ldGEAAAAAAAAAIWhkbHIAAAAAAAAAAG1kaXJhcHBsAAAAAAAAAAAAAAAAK2lsc3QAAAAjqXRvbwAAABtkYXRhAAAAAQAAAABMYXZmNTIuNzguMw=='));
+
+// Append the video to where ever you need
+document.body.appendChild(video);
